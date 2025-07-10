80d7701fdaee49b9336a689fc13668076d56443d
Ariel Quizaya Callisaya <153698653+ArielQ1@users.noreply.github.com>
2025-07-10 17:00:04 +0000

    finalizacion calse 10/7

--- ejemplo/index.py
+++ ejemplo/index.py
@@ -1,4 +1,14 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 datos = pd.read_csv('./Iris.csv')
-print(datos)
+for columna in datos.select_dtypes(include="number").columns:
+    print(f" ------- Columna: {columna}")
+    media = np.mean(datos[columna])
+    mediana = np.median(datos[columna])
+    desviacion = np.std(datos[columna])
+    print(f"Media: {media}")
+    print(f"Mediana: {mediana}")
+    print(f"Desviación estándar: {desviacion}")
+
